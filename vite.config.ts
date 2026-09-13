3240bc93056dd60035fa737ad69d81ba4fb0beea
Tom Payne <tompaynetompayne@gmail.com>
2026-09-13 17:52:19 +0100

    Deploy Meal Deal Wrapped to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  /*
+   * GitHub Pages will host this project at:
+   *
+   * https://tomp-projects.github.io/meal-deal-wrapped/
+   */
+  base: '/meal-deal-wrapped/',
 })
